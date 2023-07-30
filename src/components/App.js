75c4b311545e5dc005e32c98428a5bf40239e481
mackson <52237048+mack14@users.noreply.github.com>
2023-07-30 03:27:48 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -100,7 +100,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("https://react-quiz-backend.onrender.com")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
